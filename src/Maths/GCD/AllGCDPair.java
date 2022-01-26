9102562003bc415e0d506580dcd4b146c3951904
sachinja <sachinja@ciena.com>
2022-01-26 22:34:54 +0530

    Get original array from its all GCD pairs

--- src/Maths/GCD/AllGCDPair.java
+++ src/Maths/GCD/AllGCDPair.java
@@ -1,6 +1,8 @@
 package Maths.GCD;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /*
@@ -10,7 +12,7 @@ Find and return the original numbers which are used to calculate the GCD array i
 
 Problem Constraints
 1 <= N <= 10000
-1 <= A[i] <= 109
+1 <= A[i] <= 10^9
 
 Input Format
 The first and only argument given is the integer array A.
@@ -29,12 +31,61 @@ Output 1:[2, 8, 10]
 Output 2:[5, 15]
  */
 public class AllGCDPair {
+
     public int[] solve(int[] A) {
-        Set<Integer> answer = new HashSet<>();
+        int sizeOriginalArr = getSqrt(A.length);
+        int[] originalArr = new int[sizeOriginalArr];
+        int numberElementsFilled = 0;
+        HashMap<Integer, Integer> gcdPairFreq = new HashMap();
         for(int i=0;i<A.length;i++) {
-            answer.add(A[i]);
+            Integer occurrence = gcdPairFreq.get(A[i]);
+            if(occurrence == null) occurrence = 0;
+            gcdPairFreq.put(A[i], occurrence+1);
         }
 
-        return answer.stream().mapToInt(i->i).toArray();
+        while(numberElementsFilled < sizeOriginalArr) {
+            int nextLargestElement = findMax(gcdPairFreq);
+            decreaseFrequency(gcdPairFreq, nextLargestElement, 1);
+            originalArr[numberElementsFilled] = nextLargestElement;
+            numberElementsFilled++;
+            for(int i=numberElementsFilled-2;i>=0;i--) {
+                int gcdOfCurrentAndPrev = gcd(nextLargestElement, originalArr[i]);
+                decreaseFrequency(gcdPairFreq, gcdOfCurrentAndPrev, 2);
+            }
+        }
+
+        return originalArr;
+
+    }
+
+    private int gcd(int a, int b) {
+        if(b==0) return a;
+        return gcd(b, a%b);
+    }
+
+    private void decreaseFrequency(Map<Integer,Integer> map, int el, int decrementCount) {
+        int currentFreq = map.get(el);
+        map.put(el, currentFreq-decrementCount);
+    }
+
+    private int findMax(Map<Integer, Integer> map) {
+        int max = -1;
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int currentValFreq = entry.getValue();
+            int currentVal = entry.getKey();
+            if(currentValFreq > 0 && currentVal > max) {
+                max = currentVal;
+            }
+        }
+        return max;
+    }
+
+    private int getSqrt(int n) {
+        for(int i=1;i*i<=n;i++) {
+            if(i*i == n) {
+                return i;
+            }
+        }
+        return 0;
     }
 }
