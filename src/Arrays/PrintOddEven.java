571a306b8d7dba4f47a383642434d04870985514
Sachin Jain <sachin.jain.nsit@gmail.com>
2022-09-05 17:03:20 +0530

    Print odd even through Semaphores

---
+++ src/Arrays/PrintOddEven.java
@@ -0,0 +1,58 @@
+package Arrays;
+
+import java.util.concurrent.Semaphore;
+
+public class PrintOddEven {
+
+    private volatile int counter = 1;
+    private int MaxNumber;
+
+    private Semaphore oddSema = new Semaphore(1);
+    private Semaphore evenSema = new Semaphore(0);
+
+    private void printOdd() {
+        while(true) {
+            try {
+                oddSema.acquire();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            if (counter > MaxNumber) {
+                evenSema.release();
+                return;
+            }
+            System.out.println(counter++);
+            evenSema.release();
+        }
+    }
+
+    private void printEven()  {
+        while(true) {
+            try {
+                evenSema.acquire();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            if (counter > MaxNumber) {
+                oddSema.release();
+                return;
+            }
+            System.out.println(counter++);
+            oddSema.release();
+        }
+    }
+
+    public static void main(String[] args) {
+        PrintOddEven printer = new PrintOddEven();
+        printer.MaxNumber = 0;
+        Thread t1 = new Thread(()-> {
+            printer.printOdd();
+        });
+
+        Thread t2 = new Thread(()-> {
+            printer.printEven();
+        });
+        t1.start();
+        t2.start();
+    }
+}
